c43119ea5668bfa70caae6b818ec180dc5015a66
Lee Minhoon <hackids@naver.com>
2021-10-12 18:29:13 +0900

    init commit

--- src/components/footer/Container.js
+++ src/components/footer/Container.js
@@ -7,7 +7,7 @@ class Container extends Component {
 
 		return (
 			<div className={footer.Footer}>
-				<p>Created by <a href="https://github.com/Lee-Minhoon/Lee-Minhoon.github.io" target="_blank" rel="noopener noreferrer">React</a></p>
+				<p>Created by <a href="https://github.com/Lee-Minhoon/simple-react-resume-template" target="_blank" rel="noopener noreferrer">React</a></p>
 				<p>Last Updated at {contents.updated}</p>
 			</div>
 		);
